a2a45be4f6d22aceaafb47413c7248b70274029b
Aecio Lima <aeclean@gmail.com>
2024-11-06 17:04:59 -0300

    implements the articles list widget

--- lib/views/tabs/dashboard_tab.dart
+++ lib/views/tabs/dashboard_tab.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:laboratory/business/articles/event/articles_events.dart';
+import 'package:laboratory/business/articles/newsbloc.dart';
+import 'package:laboratory/business/articles/state/articles_state.dart';
+import 'package:laboratory/data/articles_model.dart';
 
 class DashboardTab extends StatelessWidget {
   const DashboardTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Dashboard"),
+    return BlocProvider(
+      create: (context) => ArticlesBloc()..add(LoaderEvent()),
+      child: BlocBuilder<ArticlesBloc, ArticlesState>(
+        builder: (context, state) {
+          switch (state) {
+            case LoadingState _:
+              return const Center(child: CircularProgressIndicator());
+            case SuccessState _:
+              ArticlesResponse response = state.props.first as ArticlesResponse;
+
+              return ListView.builder(
+                itemCount: (response.articles?.length ?? 0) + 1,
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                itemBuilder: (context, index) {
+                  // 20 elements
+                  // [0...19]
+                  if (index == response.articles?.length) {
+                    return Center(
+                      child: OutlinedButton(
+                          onPressed: () {}, child: Text("Load More")),
+                    );
+                  }
+
+                  ArticlesModel model =
+                      response.articles?[index] ?? ArticlesModel();
+
+                  return Column(
+                    children: [
+                      model.urlToImage != null
+                          ? Image.network(model.urlToImage ?? "")
+                          : const Center(),
+                      ListTile(
+                        title: Text(model.title ?? ""),
+                        subtitle: Text(model.description ?? ""),
+                      ),
+                    ],
+                  );
+                },
+              );
+            case ErrorState _:
+              return Center(child: Text(state.props.first.toString() ?? ""));
+            default:
+              return const Center();
+          }
+        },
+      ),
     );
   }
 }
